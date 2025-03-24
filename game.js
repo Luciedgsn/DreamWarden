c6396cc9f447da7b97cffae3281e0af1481e6c56
«Mathis-CPU» <mathis.pilon.mp@gmail.com>
2025-03-24 20:06:13 +0100

    BaseSalle1_Fonctionnelle

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 // game.js
 
 // Charger les classes
@@ -16,38 +15,3 @@ let scene1 = new Scene1(engine, canvas);
 // Lancer la boucle de rendu
 scene1.renderScene();
 scene1.resizeScene();
-
-// Ajout de l'ennemi
-const enemy = new Enemy(scene1.scene, new BABYLON.Vector3(2, 5, 0));
-
-// Créer une instance du personnage
-const personnage = new Personnage(scene1.scene, new BABYLON.Vector3(2, 3, 0));
-
-// Compter les clics pour changer de scène
-let clickCount = 0;
-
-// Configurer les actions de clic sur l'ennemi
-enemy.enemy.actionManager = new BABYLON.ActionManager(scene1.scene);
-enemy.enemy.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
-    BABYLON.ActionManager.OnPickTrigger,
-    () => {
-        clickCount++;
-        console.log(`Clic numéro: ${clickCount}`);
-
-        // Vérifier si l'utilisateur a cliqué 3 fois
-        if (clickCount === 3) {
-            console.log("Trois clics effectués. Changer de scène...");
-
-            // Supprimer la scène actuelle et libérer la mémoire
-            scene1.scene.dispose();
-            scene1 = null;  // Libérer la mémoire de la scène précédente
-
-            // Créer la nouvelle scène 2
-            const scene2 = new Scene2(engine, canvas);
-
-            // Lancer la boucle de rendu de la scène 2
-            scene2.renderScene();
-            scene2.resizeScene();
-        }
-    }
-));
